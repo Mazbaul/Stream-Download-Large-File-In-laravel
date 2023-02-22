d0f24be09e33ab361878dc2c086a9638b7a6f73b
Mazbaul <mazbaul17@gmail.com>
2023-02-22 10:12:47 +0600

    done

--- resources/views/StudentEdit.blade.php
+++ resources/views/StudentEdit.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('title', 'Add Student')
+@section('title', 'Edit Student')
 
 @section('content')
 <div class="content">
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="card">
-                <div class="card-header"><strong class="card-title">Add Student</strong></div>
+                <div class="card-header"><strong class="card-title">Edit Student</strong></div>
                 <div class="card-body">
                     <form class="form-horizontal" method="POST" action="{{ route('student-update',[$student->id]) }}">
                         {{ csrf_field() }}
